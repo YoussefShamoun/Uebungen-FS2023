c08f6cc7f020f58f1b0bb2b9fd2e7a076816369b
Youssef Shamoun <youssefshamoun7@gmail.com>
2023-04-04 16:08:51 +0200

    Update Uebung 04.py

--- Übungen/Kapitel 04/Uebung 04.py
+++ Übungen/Kapitel 04/Uebung 04.py
@@ -1,51 +1,60 @@
 from PyQt5.QtWidgets import *
-import os
 
 
 class Fenster(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("GUI-Programmierung")  # Fenster-Titel setzen
 
+
+        self.setWindowTitle("GUI-Programmierung")                                     # Fenster-Titel setzen
+#------------------------------------------------------------------------------
+        layout = QFormLayout()                                          # Layout erstellen
+#------------------------------------------------------------------------------
+   
         menubar = self.menuBar()
-        filemenu = menubar.addMenu("File")
+        filemenu = menubar.addMenu("File")                              # File-Menu erstellen
+        viewmenu = menubar.addMenu("View")                              # View-Menu erstellen
 
-        save = QAction("Save", self)
-        save.triggered.connect(self.save_data)  # Connect save action to save_data method
+#------------------------------------------------------------------------------
+      
+        save = QAction("Save", self)                                    # Action definieren
+        save_as = QAction("Save as", self)
         quit = QAction("Quit", self)
-        quit.triggered.connect(self.close)  # Connect quit action to close method of the window
-        self.button = QPushButton("Save")
-        self.button.clicked.connect(self.save_data)  # Connect button click to save_data method
+        zoom_in = QAction ("Zoom in", self)
+        zoom_out = QAction ("Zoom out", self)
 
-        quit.setMenuRole(QAction.QuitRole)   # Rolle "beenden" zuweisen, nur für MacOS relevant
-
-        filemenu.addAction(save)
-        filemenu.addSeparator()
+        filemenu.addAction(save)                                        # Action zum File-Menu hinzufügen
+        filemenu.addAction(save_as)
         filemenu.addAction(quit)
+        viewmenu.addAction(zoom_in)                                     # Action zum View-Menu hinzufügen
+        viewmenu.addAction(zoom_out)
 
-        # Layout erstellen:
-        layout = QFormLayout()
+#------------------------------------------------------------------------------           
 
-        # Widget-Instanzen erstellen:
-
-        self.vornameLineEdit = QLineEdit()
-        self.nameLineEdit = QLineEdit()
+        self.vorname = QLineEdit()                                      # Widget-Instanzen erstellen
+        self.name = QLineEdit()
         self.geburtsdatum = QDateEdit()
-        self.adresseLineEdit = QLineEdit()
-        self.postleitzahlLineEdit = QLineEdit()
-        self.ortLineEdit = QLineEdit()
+        self.adresse = QLineEdit()
+        self.postleitzahl = QLineEdit()
+        self.ort = QLineEdit()
         self.countries = QComboBox()
         self.countries.addItems(["Schweiz", "Deutschland", "Österreich"])
+        self.button = QPushButton("Save")                               # Save button erstellen
 
-        # Layout füllen:
-        layout.addRow("Vorname:", self.vornameLineEdit)
-        layout.addRow("Name:", self.nameLineEdit)
+        layout.addRow("Vorname:", self.vorname)                         # Instanzen zum Layout hinzufuegen
+        layout.addRow("Name:", self.name)
         layout.addRow("Geburtsdatum:", self.geburtsdatum)
-        layout.addRow("Adresse:", self.adresseLineEdit)
-        layout.addRow("PostLeitzahl:", self.postleitzahlLineEdit)
-        layout.addRow("Ort:", self.ortLineEdit)
+        layout.addRow("Adresse:", self.adresse)
+        layout.addRow("PostLeitzahl:", self.postleitzahl)
+        layout.addRow("Ort:", self.ort)
         layout.addRow("Land:", self.countries)
         layout.addRow(self.button)
+
+        save.triggered.connect(self.save_data)                          # Connect save action to save_data method
+        quit.triggered.connect(self.close)                              # Connect quit action to close method of the window
+        self.button.clicked.connect(self.save_data)                     # Connect button click to save_data method
+
+#------------------------------------------------------------------------------
 
         center = QWidget()
         center.setLayout(layout)
@@ -53,12 +62,14 @@ class Fenster(QMainWindow):
         self.setCentralWidget(center)
         self.show()
 
-    def save_data(self):
-        data = f"{self.vornameLineEdit.text()},{self.nameLineEdit.text()},{self.geburtsdatum.text()},{self.adresseLineEdit.text()},{self.postleitzahlLineEdit.text()},{self.ortLineEdit.text()},{self.countries.currentText()}"
-        file = open("output.txt", "w",encoding="utf-8") 
-        file.write(str(data))
-       
+#------------------------------------------------------------------------------
 
+    def save_data(self):                                                # save_data Methode definieren (Text + Datei erstellen + Daten Einfuegen)
+        data = f"{self.vorname.text()},{self.name.text()},{self.geburtsdatum.text()},{self.adresse.text()},{self.postleitzahl.text()},{self.ort.text()},{self.countries.currentText()}"
+        file = open("output.txt", "w",encoding="utf-8") 
+        file.write(data)
+
+#------------------------------------------------------------------------------
 
 app = QApplication([])
 win = Fenster()
